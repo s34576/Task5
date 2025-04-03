c0eb66658b4f8e52182d7fb9b0a9940d966919ae
s34576 <s34576@pjwstk.edu.pl>
2025-04-04 01:55:43 +0200

    Zmiana 1

--- Zadanie5/src/SystemRezerwacji.java
+++ Zadanie5/src/SystemRezerwacji.java
@@ -3,6 +3,28 @@ import java.util.ArrayList;
 public class SystemRezerwacji {
     private ArrayList<Wydarzenie> listaRezerwacji= new ArrayList<>();
     private ArrayList<Klient> listaKlientow=new ArrayList<>();
+    public void dokonajRezerwacji(Klient k, Wydarzenie w) {
+        this.listaKlientow.add(k);
+        this.listaRezerwacji.add(w);
+        w.zarezerwujMiejsce();
+        k.dodajRezerwacje(w);
+    }
+    public void zmienCeneWydarzenia(String nazwa, double nowaCena){
+        Wydarzenie w = znajdzWydarzenie(nazwa);
+        if(w == null){
+            throw new IllegalArgumentException("Wydarzenie nie istnieje");}
+        w.setCena(nowaCena);
+    }
+
+    public Klient znajdzKlienta(String nazwisko)
+   {
+       for(Klient k: listaKlientow)
+       {
+           if(k.getNazwisko().equals(nazwisko))
+               return k;
+       }
+       return null;
+   }
     public Wydarzenie znajdzWydarzenie(String nazwa) {
         for(Wydarzenie w : listaRezerwacji)
         {
